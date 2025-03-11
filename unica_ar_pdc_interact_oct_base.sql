816a8277a21a77cef351f37dcee318f70e6c2f22
yogesh9602 <55808913+yogesh9602@users.noreply.github.com>
2025-03-11 22:40:56 +0530

    Add files via upload

---
+++ unica_ar_pdc_interact_oct_base.sql
@@ -0,0 +1,10 @@
+select
+cif_id,
+dt,
+etl_created_by,
+etl_created_time,
+etl_last_updated_by,
+etl_last_updated_time,
+etl_change_flag,
+wf_audit_id
+from srcl.unica_ar_pdc_interact_oct_base
---
+++ unica_blacklist.sql
@@ -0,0 +1,10 @@
+select
+crn_prospect_id,
+blacklist_date,
+etl_created_by,
+etl_created_time,
+etl_last_updated_by,
+etl_last_updated_time,
+etl_change_flag,
+wf_audit_id
+from srcl.unica_blacklist
